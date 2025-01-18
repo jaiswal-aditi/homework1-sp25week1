12b17d92c35791eed7f62c04274d9270da0225b9
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-01-17 19:11:55 -1000

    Update prob3.c

--- prob3.c
+++ prob3.c
@@ -4,11 +4,11 @@
 Write a C Program that gets f1, f2, x1 and x2 as input
 from the keyboard and prints them in the following format:
 
-Enter values for f1 and f2:
+Enter values for f1 and f2:    // this statement should be printed first before inputting any value
 39.6 15.667
 Enter value for x1 and x2:
 44 7
-f1 = 39.600000, f2 = 15.667
+f1 = 39.600000, f2 = 15.667     
 x1 = 44, x2 = 7 
 */
 
