94998601b9adbe8f6b453b5f0e370af1decbd61a
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-01-17 18:42:00 -1000

    Update prob4e.c

--- prob4e.c
+++ prob4e.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
-Reading 4 integers in a single scanf statement and print them (using a single
-prinJ statement) in separate lines such that the last digit of each integer 
+Read 4 integers in a single scanf statement and print them (using a single
+prinf statement) in separate lines such that the last digit of each integer 
 is exactly 10 spaces away from the beginning of the line it is printed in (the 9 spaces before will be occupied by blanks or other digits of the integer).
 Remember that different integers can have different number of digits.
 */
