93ca0318504db555014039716b518c23fd72341c
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-01-17 18:40:33 -1000

    Update prob4a.c

--- prob4a.c
+++ prob4a.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
-Using scanf() to read two integers and two floating point numbers, each in a
-separate statement (so 4 scanfs) and print them separately using printf (4
-printfs) with some nice comments.
+Read two integers and two floating point numbers, each in a separate scanf()
+statement (so 4 scanf’s) and print them with separate printf statements (4 printf’s)
+with some nice message.
 */
 
 int main() {
-    
+    // your code
     return 0;
 }
