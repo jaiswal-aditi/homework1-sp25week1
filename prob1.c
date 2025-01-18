3ab7018eb76e52f7d1d31af85a0126e9f6ca781a
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-01-17 18:37:12 -1000

    Create prob1.c

---
+++ prob1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+Write a C Program that prints the following output:
+Aloha World! Aloha
+Aditi!      
+
+Please replace Aditi with your first name. Also make sure the format is same (your name should be in new line).
+*/
+
+int main() {
+  // your code
+    return 0;
+}
